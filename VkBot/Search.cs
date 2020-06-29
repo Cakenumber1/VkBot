ecf0f141a260f786cd826e8954e05c970d490239
M <cakenumber1@mail.ru>
2020-06-29 10:54:47 +0300

    uf

--- VkBot/Search.cs
+++ VkBot/Search.cs
@@ -231,7 +231,7 @@ namespace VkBot
                         curUrl += "?sort=-sell_course_4";
                     }
                     CQ dom0 = CQ.CreateFromUrl(curUrl);
-                    answ.Add(curUrl);
+                    //answ.Add(curUrl);
                     int tr = 0;
                     foreach (IDomObject obj in dom0.Find("td"))
                     {
